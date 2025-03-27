fc0cbf24396ffac77ddf5a157eb99a51dce1ff99
Frumen Olivas <frumen@olivas.com>
2025-03-27 15:24:10 -0600

    27 marzo

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -4,11 +4,10 @@ window.onload = () => {
   } else {
     const menuGame = document.getElementById("menuGame");
     menuGame.setAttribute("href", "./game.html?id=" + sessionStorage.id);
+	const h1 = document.getElementById("saludo");
+	h1.innerHTML = "hola " + sessionStorage.name;
   }
 };
-
-const h1 = document.getElementById("saludo");
-h1.innerHTML = "hola " + sessionStorage.name;
 
 const logout = document.getElementById("logout");
 logout.addEventListener("click", () => {
